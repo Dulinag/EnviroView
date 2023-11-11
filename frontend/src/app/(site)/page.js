73c1ec938c219582b56f69e302d292bf5b84d5ea
Dulina <dulinagunasekara@gmail.com>
2023-11-10 21:52:07 -0500

    started

---
+++ frontend/src/app/(site)/page.js
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+<>
+
+<div class="flex justify-center items-center h-screen bg-sky-500">
+  <div class="text-2xl">EnviroView</div>
+</div>
+
+
+
+</>
+  )
+}
